589aeacff01a5a41a02aaac36def3478d65d2427
FariaOaishi <oaishi.faria@gmail.com>
2018-05-04 05:30:23 +0600

    chatbot finished

--- app/src/main/java/com/example/fariahuq/tori/Demo.java
+++ app/src/main/java/com/example/fariahuq/tori/Demo.java
@@ -4,9 +4,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.github.bassaer.chatmessageview.model.ChatUser;
@@ -15,7 +17,11 @@ import com.github.bassaer.chatmessageview.util.ChatBot;
 import com.github.bassaer.chatmessageview.view.ChatView;
 import com.github.bassaer.chatmessageview.model.IChatUser;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
 public class Demo extends AppCompatActivity {
 
@@ -43,9 +49,9 @@ public class Demo extends AppCompatActivity {
         mChatView = (ChatView)findViewById(R.id.chat_view);
 
         //Set UI parameters if you need
-        mChatView.setRightBubbleColor(ContextCompat.getColor(this, R.color.green500));
+        mChatView.setRightBubbleColor(ContextCompat.getColor(this, R.color.Chatcolor));
         mChatView.setLeftBubbleColor(Color.WHITE);
-        mChatView.setBackgroundColor(ContextCompat.getColor(this, R.color.blueGray500));
+        mChatView.setBackgroundColor(ContextCompat.getColor(this, R.color.ChatBackground));
         mChatView.setSendButtonColor(ContextCompat.getColor(this, R.color.cyan900));
         mChatView.setSendIcon(R.drawable.ic_action_send);
         mChatView.setRightMessageTextColor(Color.WHITE);
@@ -56,6 +62,13 @@ public class Demo extends AppCompatActivity {
         mChatView.setInputTextHint("new message...");
         mChatView.setMessageMarginTop(5);
         mChatView.setMessageMarginBottom(5);
+
+        final Message receivedMessage = new Message.Builder()
+                .setUser(you)
+                .setRight(false)
+                .setText("Hi , Do you want to chat?")
+                .build();
+        mChatView.receive(receivedMessage);
 
         //Click Send Button
         mChatView.setOnClickSendButtonListener(new View.OnClickListener() {
@@ -73,17 +86,37 @@ public class Demo extends AppCompatActivity {
                 //Reset edit text
                 mChatView.setInputText("");
 
+                String msg = "Type Something";
+
+                if(message.getText().equals("yesterday my father died"))
+                {
+                    msg = "pal, i'm sorry for your loss...it's okay to feel low, to feel " +
+                            "angry that he is not here, to feel hard to accept that he wont be able " +
+                            "to keep his hand on your head, you can share your feeling with me, pal.";
+                }
+                else if(message.getText().equals("yeah i'm ready"))
+                {
+                    msg = "I really appreciate your co-operation. Do you feel hard sometimes " +
+                            "to accept the reality of the loss? Like , \"why is this happening to me?\"";
+                }
+                else if(message.getText().equals("no i don't feel good"))
+                {
+                    msg = "It's okay pal. You know grief is a natural response to losing someone you care about." +
+                            "There's no right or wrong way to grieve. The important thing is to let " +
+                            "yourself grieve and mourn as much and as long as you need to. " +
+                            "Do you want to hear a story of one of my friends?";
+                }
                 //Receive message
                 final Message receivedMessage = new Message.Builder()
                         .setUser(you)
                         .setRight(false)
-                        .setText("Type Something")
+                        .setText(msg)
                         .build();
 
                 //ChatBot.talk(me.getName(), message.getText())
                 // This is a demo bot
                 // Return within 3 seconds
-                int sendDelay = (new Random().nextInt(4) + 1) * 1000;
+                int sendDelay = (new Random().nextInt(3) + 1) * 1000;
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
